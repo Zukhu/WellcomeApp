c7d66e810ecf2da11eec2a7c07bc5496bf6d6da7
unknown <oerstedico@gmail.com>
2020-04-08 19:44:23 +0200

    basic stylesheet

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,4 +1,9 @@
 window.onload = function() {
+
+    /* ------------------------- 
+    ----DOM elements creation--- 
+    --------------------------- */
+
     var footer = document.createElement('footer');
     footer.setAttribute('id', 'author-id');
     //Returns the <body> element
@@ -8,4 +13,29 @@ window.onload = function() {
     footer_text.innerHTML = "Zukhu © 2020";
     footer.appendChild(footer_text);
 
+
+
+    /* ------------------------- 
+    ---------Stylesheet--------- 
+    --------------------------- */
+
+    //Returns the <html> element
+    document.documentElement.style.height = "100%";
+    document.documentElement.style.margin = "0";
+    document.documentElement.style.padding = "0";
+
+    document.body.style.minHeight = "100%";
+    document.body.style.margin = "0";
+    document.body.style.padding = "0";
+    document.body.style.backgroundColor = "black";
+
+    footer.style.position = "absolute";
+    footer.style.backgroundColor = "gray";
+    footer.style.bottom = "0";
+    footer.style.left = "0";
+    footer.style.right = "0";
+    footer.style.marginTop = "7vw";
+
+    footer_text.style.fontSize = "5rem";
+    footer_text.style.color = "white";
 }
